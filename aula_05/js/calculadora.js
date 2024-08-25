d1eebdd693e53738f6ca05b76032a5c35fc79451
Julia Fitzlaff <julia.fitzlaff@unidavi.edu.br>
2024-08-25 14:51:50 -0300

    atualização

--- aula_05/js/calculadora.js
+++ aula_05/js/calculadora.js
@@ -6,6 +6,33 @@ function num2() {
     return parseFloat(document.getElementById("num2").value);
 }
 
-function soma() {
-    informaResultado(num1() + num2());
+function somar() {
+    return (num1() + num2());
+}
+
+function subtrair() {
+    return (num1() - num2());
+}
+
+function multiplicar() {
+    return (num1() * num2());
+}
+
+function dividir() {
+    return (num1() / num2());
+}
+
+function resultado() {
+    let operador = document.getElementById("operador");
+    let result;
+        if (operador.value == 1) {
+            result = somar();
+        } else if (operador.value  == 2) {
+            result = subtrair();
+        } else if (operador.value  == 3) {
+            result = multiplicar();
+        } else if (operador.value  == 4) {
+            result = dividir();
+        }
+        document.getElementById("resultado").innerHTML = result;  
 }
