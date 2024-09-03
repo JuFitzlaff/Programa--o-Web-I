0dcd72989fd236973b66429c800d7846bb46a9a7
Julia Fitzlaff <julia.fitzlaff@unidavi.edu.br>
2024-09-03 20:57:12 -0300

    atualização

--- aula_05/js/mediaAlunos.js
+++ aula_05/js/mediaAlunos.js
@@ -1,12 +1,11 @@
 function mediaNotas() {
-
-let media = new Array;
-let matrizNotas = [ [10,8,9,6,8.7,10,6,7,9],
-                    [10,3,1,7,9,8.5,0,1,10],
-                    [3,7,3,10,10,9,1,2,8],
-                    [10,9,8,7,6,2,1,10,8],
-                    [10,8,9,7,6,3,1,10,9.5],
-                    [10,9,10,8,7,10,6,10,6] ];
+    let media = new Array;
+    let matrizNotas = [ [10,8,9,6,8.7,10,6,7,9],
+                        [10,3,1,7,9,8.5,0,1,10],
+                        [3,7,3,10,10,9,1,2,8],
+                        [10,9,8,7,6,2,1,10,8],
+                        [10,8,9,7,6,3,1,10,9.5],
+                        [10,9,10,8,7,10,6,10,6] ];
 
     for(let col=0; col <= 8; col++) {
         let soma = 0;
@@ -15,5 +14,16 @@ let matrizNotas = [ [10,8,9,6,8.7,10,6,7,9],
         }
         media[col] = soma / 6;
     }
-    document.getElementById("demo").innerHTML = media;
+
+    let tabela = document.getElementById("tabela");
+    let linhaMedia = tabela.insertRow(-1); 
+
+    let celula = linhaMedia.insertCell(0);
+    celula.innerHTML = "Média por Nota";
+
+    for(let i = 0; i <= 8; i++) {
+       let celula = linhaMedia.insertCell(i + 1);
+            celula.innerHTML = media[i].toFixed(2);
+   }
 }
+
