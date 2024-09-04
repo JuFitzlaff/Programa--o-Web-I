77ebb17646363f660ef28d10b2cf9d9b35e05f1f
Julia Fitzlaff <julia.fitzlaff@unidavi.edu.br>
2024-09-03 21:21:48 -0300

    atualização

--- aula_05/js/mediaAlunos.js
+++ aula_05/js/mediaAlunos.js
@@ -1,5 +1,5 @@
 function mediaNotas() {
-    let media = new Array;
+    let media = [];
     let matrizNotas = [ [10,8,9,6,8.7,10,6,7,9],
                         [10,3,1,7,9,8.5,0,1,10],
                         [3,7,3,10,10,9,1,2,8],
@@ -14,16 +14,19 @@ function mediaNotas() {
         }
         media[col] = soma / 6;
     }
+    return media;
+}
 
+function apresentaMediaNotas() {
+    let media = mediaNotas();
     let tabela = document.getElementById("tabela");
     let linhaMedia = tabela.insertRow(-1); 
-
     let celula = linhaMedia.insertCell(0);
+
     celula.innerHTML = "Média por Nota";
 
     for(let i = 0; i <= 8; i++) {
-       let celula = linhaMedia.insertCell(i + 1);
+        let celula = linhaMedia.insertCell(i + 1);
             celula.innerHTML = media[i].toFixed(2);
    }
 }
-
